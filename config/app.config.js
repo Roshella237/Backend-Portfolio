d7af8049d2903f4130532f6b70b9952c1d8bc1e7
Muyang Roshella <muyangroshella40@gmail.com>
2023-06-23 14:48:27 +0100

    Completed the backend

---
+++ config/app.config.js
@@ -0,0 +1,11 @@
+
+require('dotenv').config;
+
+const MONGO_DB_CONFIG = {
+    DB: 'mongodb+srv://muyangroshella40:TuQgZctrsOZOmeWB@reactbackendportfolio.f9f6h8s.mongodb.net/ReactBackendProject?retryWrites=true&w=majority'
+}
+
+
+module.exports = {
+    MONGO_DB_CONFIG
+}
